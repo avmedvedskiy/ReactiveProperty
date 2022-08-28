ba9014b82c9e216833809f39424daaa2ba305bc4
avmedvedskiy <a.v.medvedskiy@gmail.com>
2022-08-28 10:14:03 +0300

    fix dropdown in sync property when parent is managed reference

--- Editor/SerializedPropertyExtensions.cs
+++ Editor/SerializedPropertyExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Reflection;
 using UnityEditor;
 
@@ -11,10 +12,11 @@ namespace MVVM.Editor
             var info = property.GetFieldInfo();
             Object obj = property.GetTargetObject();
             var value = info.GetValue(obj);
-            var methodInfo = value.GetType().GetMethod(methodName,BindingFlags.Public| BindingFlags.NonPublic | BindingFlags.Instance);
+            var methodInfo = value.GetType().GetMethod(methodName,
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
             methodInfo.Invoke(value, Array.Empty<object>());
         }
-        
+
         public static FieldInfo GetFieldDeep(this Type type, string fieldName)
         {
             var field = type.GetField(fieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
@@ -26,32 +28,68 @@ namespace MVVM.Editor
         private static FieldInfo GetFieldInfo(this SerializedProperty property)
         {
             string[] fields = property.propertyPath.Split('.');
-            Type typeFromProperty = GetScriptTypeFromProperty(property);
-            FieldInfo info = null;
-            foreach (var fName in fields)
+            int i = 0;
+            Type searchedType;
+            var parentManagedReferenceType = property.GetParentManagedReferenceType();
+            if(parentManagedReferenceType == null)
+                searchedType = property.GetScriptTypeFromProperty();
+            else
             {
-                info = typeFromProperty.GetFieldDeep(fName);
-                typeFromProperty = info.FieldType;
+                searchedType = parentManagedReferenceType;
+                i = 1;//ignore first element, because first element getted in parent property if it managed reference
+            }
+            
+            FieldInfo info = null;
+            for (; i < fields.Length; i++)
+            {
+                var fName = fields[i];
+                info = searchedType.GetFieldDeep(fName);
+                searchedType = info.FieldType;
             }
             return info;
+
         }
-        
+
+        private static Type GetParentManagedReferenceType(this SerializedProperty property)
+        {
+            string[] fields = property.propertyPath.Split('.');
+            var parentProperty = property.serializedObject.FindProperty(fields.First());
+            return parentProperty?.GetManagedReferenceType();
+        }
+
+        public static Type GetManagedReferenceType(this SerializedProperty property)
+        {
+            if (property.propertyType != SerializedPropertyType.ManagedReference)
+            {
+                return null;
+            }
+
+            return GetType(property.managedReferenceFullTypename);
+        }
+
+        static Type GetType(string typeName)
+        {
+            int splitIndex = typeName.IndexOf(' ');
+            var assembly = Assembly.Load(typeName.Substring(0, splitIndex));
+            return assembly.GetType(typeName.Substring(splitIndex + 1));
+        }
+
         public static Type GetTargetType(this SerializedProperty property)
         {
             return property.GetFieldInfo().FieldType;
         }
-        
+
         public static Type GetGenericTypeArguments(this SerializedProperty property)
         {
             var type = property.GetTargetType();
             return type.GenericTypeArguments.Length > 0 ? type.GenericTypeArguments[0] : null;
         }
-        
+
         public static Type GetScriptTypeFromProperty(this SerializedProperty property)
         {
             return property.serializedObject.targetObject.GetType();
         }
-        
+
         public static Object GetTargetObject(this SerializedProperty property)
         {
             return property.serializedObject.targetObject;
