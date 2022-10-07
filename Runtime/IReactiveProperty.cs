f8cc2897c47a0eca5c2559e9a769b23da856396e
avmedvedskiy <a.v.medvedskiy@gmail.com>
2022-10-07 12:59:50 +0300

    add ireadonlyproperty interface

--- Runtime/IReactiveProperty.cs
+++ Runtime/IReactiveProperty.cs
@@ -9,8 +9,15 @@ namespace MVVM
     public interface IReactivePropertyValue : IReactiveProperty
     {
     }
+    
+    public interface IReadOnlyReactiveProperty<out T> : IReactivePropertyValue
+    {
+        T Value { get; }
+        void Subscribe(Action<T> action);
+        void UnSubscribe(Action<T> action);
+    }
 
-    public interface IReactiveProperty<T> : IReactivePropertyValue
+    public interface IReactiveProperty<T> : IReadOnlyReactiveProperty<T>
     {
         T Value { get; set; }
         void Subscribe(Action<T> action);
