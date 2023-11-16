a9c12f099e03552285cd891e170bf21cfbbb24e5
avmedvedskiy <a.v.medvedskiy@gmail.com>
2023-11-16 14:18:45 +0200

    add TryGetIndex extensions to list

--- Runtime/List/ReactiveListExtensions.cs
+++ Runtime/List/ReactiveListExtensions.cs
@@ -1,7 +1,25 @@
+using System;
+using System.Collections.Generic;
+
 namespace MVVM.Collections
 {
     public static class ReactiveListExtensions
     {
+        public static bool TryGetIndex<T>(this IList<T> list, Predicate<T> match, out int index)
+        {
+            int count = list.Count;
+            for (int i = 0; i < count; i++)
+            {
+                if (match(list[i]))
+                {
+                    index = i;
+                    return true;
+                }
+            }
+            index = -1;
+            return false;
+        }
+        
         public static void Subscribe<T>(this ReactiveList<T> list, IReactiveListEventHandler<T> listener)
         {
             list.OnAdd += listener.OnAdd;
