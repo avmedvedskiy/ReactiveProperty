bec6fd7b4c10b81c361b28854c9864949860cd5f
avmedvedskiy <a.v.medvedskiy@gmail.com>
2024-05-22 14:51:41 +0300

    change to IReadOnly

--- Runtime/Extensions/ReactivePropertyExtensions.cs
+++ Runtime/Extensions/ReactivePropertyExtensions.cs
@@ -2,7 +2,10 @@ namespace MVVM
 {
     public static class ReactivePropertyExtensions
     {
-        public static void FromProperty<T>(this ReactiveProperty<T> source, ReactiveProperty<T> reactiveProperty)
+        /// <summary>
+        /// Not useful now, memory leaked
+        /// </summary>
+        public static void FromProperty<T>(this ReactiveProperty<T> source, IReadOnlyReactiveProperty<T> reactiveProperty)
         {
             source.Value = reactiveProperty.Value;
             reactiveProperty.Subscribe(x => source.Value = x);
